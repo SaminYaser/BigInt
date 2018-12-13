0a4bc73c6ce951c89d6bc0c8126dbb8de7a4c8a4
Samin Yaser <saminyaser.24csedu.016@gmail.com>
2018-12-14 00:47:56 +0600

    Basic Constructor and Summation done Working on Substraction

--- BigInt/Source.cpp
+++ BigInt/Source.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+#include "BigInt.h"
 
 int main()
 {
-	std::cout << "Print Test" << std::endl;
+	int ca = 1;
+	double ba = -1;
+
+	BigInt a("9"), b("-01"), c;
+
+	c = a.add(1);
+	c.PRINT();
+
+	int pagla;
+	std::cin >> pagla;
 	return 0;
 }
